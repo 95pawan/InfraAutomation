3cf35019170095c06a7f52fe61702eeada712a8a
95pawan <36891609+95pawan@users.noreply.github.com>
2019-06-11 10:53:23 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -64,6 +64,17 @@ resource "azurerm_network_security_group" "test" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
+	security_rule {
+        name                       = "httpp"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8081"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 	
 }
 
